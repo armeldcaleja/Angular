5261480407f72d7581f9eb4daacca67ed38b840e
armeldcaleja <armel.d.caleja@accenture.com>
2017-10-23 02:47:42 +0800

    http

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -10,6 +9,11 @@ import { DashboardComponent } from './dashboard.component';
 
 import { HeroService } from './hero.service';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpModule } from '@angular/http';
+
+// imports for loading and configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -21,6 +25,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [HeroService],
--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -5,12 +5,6 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService{
-    //--------- code without promise ----------//
-    // getHeroes(): Hero[]{
-    //     return HEROES;
-    // }
-
-    //--------- code for promise ----------//
 
     getHeroes(): Promise<Hero[]>{
         return Promise.resolve(HEROES);
